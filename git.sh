d5c589e58af6064195fdb25b998ffd65df1f6725
Hector Jusforgues <hector.jusforgues@gmail.com>
2017-04-14 11:18:05 +0200

    Fix debug messages

--- git.sh
+++ git.sh
@@ -5,11 +5,11 @@ echo "machine $DRONE_NETRC_MACHINE" >> "$HOME/.netrc"
 echo "login $DRONE_NETRC_USERNAME" >> "$HOME/.netrc"
 echo "password $DRONE_NETRC_PASSWORD" >> "$HOME/.netrc"
 
-echo "## Setting up workspace @ ${DRONE_WORKSPACE}"
 if [ -z "$DRONE_WORKSPACE" ]; then
     DRONE_WORKSPACE=$(pwd)
 else
     if [ ! -d "$DRONE_WORKSPACE" ]; then
+        echo "## Creating workspace (${DRONE_WORKSPACE})"
         mkdir -p "$DRONE_WORKSPACE"
     fi
 fi
@@ -33,6 +33,6 @@ else
     fi
 fi
 
-echo "## Exporting ${DRONE_COMMIT_SHA} to workspace"
+echo "## Exporting ${DRONE_COMMIT_SHA} to workspace (${DRONE_WORKSPACE})"
 set -e
 git archive --format=tar "$DRONE_COMMIT_SHA" | (cd "$DRONE_WORKSPACE" && tar xf -)
